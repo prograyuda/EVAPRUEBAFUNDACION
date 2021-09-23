1c9a93623e13f32f9b68980f503c406e6f687e14
prograyuda <72632824+prograyuda@users.noreply.github.com>
2021-09-23 16:01:52 -0600

    dsadsa

--- js/menu.js
+++ js/menu.js
@@ -1,2 +1,34 @@
+console.log('hello world');
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
 
+(function () {
+  const MENU_OBJ = {
+    init: function () {
+      console.log('cargando javascript menu...');
+
+      this.eventHandlers();
+    },
+
+    eventHandlers: function () {
+      document.querySelector('.menu-btn').addEventListener('click', function(){
+        document.querySelector('.menu-navegacion').classList.toggle('spread');
+      });
+    
+
+    }
  
+
+  }
+
+  MENU_OBJ.init();
+})();
