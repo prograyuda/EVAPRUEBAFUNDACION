8f95a442559358083e2c93c2b8addaf6c93f0f65
prograyuda <72632824+prograyuda@users.noreply.github.com>
2021-09-22 20:46:39 -0600

    ds

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,2 @@
-(function () {
-    const MAIN_OBJ = {
-      init: function () {
-        console.log('cargando menu js');
-  
-        this.eventHandlers();
-      },
-  
-      eventHandlers: function () {
-        document.querySelector('.Lineas').addEventListener('click', function(){
-          document.querySelector('.Contenedor').classList.toggle('abrir');
-        });
-  
-      }
-    }
-    MAIN_OBJ.init();
-  })();
+
  
